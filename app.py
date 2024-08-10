57eee3152756541193536ba2b44c6d0ed9077fcf
Prabal Singh <41672757+prblsing@users.noreply.github.com>
2024-08-10 07:02:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ st.title('Watch What - Movie Recommendation System')
 # put your search query here
 query = st.text_input('Search for a movie', value="Enter your filter query here")
 
+# dropdown to select the number of recommendations
+recommendation_count = st.selectbox('Number of recommendations to display:', options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], index=4)
+
+
 if st.button('Get Recommendations'):
     # perform the search using the comprehensive search function
     recommendations = comprehensive_search(query)
