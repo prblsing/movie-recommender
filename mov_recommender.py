311e51fc2e3275d1fc3540927cc4caa84fa06491
Prabal Singh <41672757+prblsing@users.noreply.github.com>
2024-08-10 06:51:46 +0530

    Update mov_recommender.py

--- mov_recommender.py
+++ mov_recommender.py
@@ -4,12 +4,12 @@ import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 
-# File paths for embeddings, index, and TF-IDF vectorizer
+# file paths for embeddings, index, and TF-IDF vectorizer
 embeddings_file = 'embeddings.pkl'
 tfidf_vectorizer_file = 'tfidf_vectorizer.pkl'
 metadata_file = 'movie_metadata.pkl'
 
-# Load precomputed embeddings, TF-IDF vectorizer, and movie metadata
+# load precomputed embeddings, TF-IDF vectorizer, and movie metadata
 with open(embeddings_file, 'rb') as f:
     sentence_embeddings = pickle.load(f)
 with open(tfidf_vectorizer_file, 'rb') as f:
@@ -30,16 +30,16 @@ def comprehensive_search(query, sbert_weight=0.7, k=5):
     - DataFrame: The top k movies that match the query along with their release years.
     """
 
-    # Generate embeddings for the query using Sentence-BERT
+    # generate embeddings for the query using Sentence-BERT
     sentence_model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
     query_embedding = sentence_model.encode([query]).astype('float32')
 
-    # Compute cosine similarity between the query and all movie embeddings
+    # compute cosine similarity between the query and all movie embeddings
     similarities = cosine_similarity(query_embedding, sentence_embeddings)[0]
 
-    # Get top k most similar movies, usually 5
+    # get top k most similar movies, usually 5
     top_indices = similarities.argsort()[-k:][::-1]
     top_movies = movie_metadata.iloc[top_indices]
 
-    # Return the relevant columns
+    # return the relevant columns
     return top_movies[['Series_Title', 'Genre', 'Released_Year', 'Overview', 'Director', 'Star1']], top_movies['Released_Year']
