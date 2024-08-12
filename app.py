8b6e783f09233a4b20f67a1fe3901683fd4cb6b8
Prabal Singh <41672757+prblsing@users.noreply.github.com>
2024-08-12 07:49:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title('Just Another Movie Recommendation System')
 query = st.text_input('Search for a movie', value="Enter your filter query here")
 
 # dropdown to select the number of recommendations
-recommendation_count = st.selectbox('Number of recommendations to display:', options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], index=4)
+recommendation_count = st.selectbox('Number of recommendations to display', options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], index=4)
 
 
 if st.button('Get Recommendations'):
